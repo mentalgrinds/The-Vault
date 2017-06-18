98c97d988f5350771c10cbb6b2699de414ab4311
Jesse Copeland <jesse.copeland@gmail.com>
2017-06-17 15:46:40 -1000

    Complete

--- vault.js
+++ vault.js
@@ -1,4 +1,21 @@
 'use strict';
 module.exports = function() {
-  
+
+  let vault = {};
+
+  function setValue(key, value) {
+    vault[key] = value;
+  }
+
+  function getValue(key) {
+    if (vault.hasOwnProperty(key)) {
+      return vault[key];
+    }
+    return null;
+  }
+
+  return {
+    setValue: setValue,
+    getValue: getValue
+  }
 };
